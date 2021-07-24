6bc9e812a76d17e4650536104016b2b5d01dc27f
Nelly Torque <nesavwa@gmail.com>
2021-07-24 11:51:59 +0800

    1st commit

---
+++ Develop/index.js
@@ -0,0 +1,22 @@
+// TODO: Include packages needed for this application
+
+const Prompt = require("inquirer/lib/prompts/base");
+
+// TODO: Create an array of questions for user input
+const questions = [
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+
+  prompt(userquestions).then(answer => {
+      const response = generateMarkdown
+  })
+}
+
+// Function call to initialize app
+init();
